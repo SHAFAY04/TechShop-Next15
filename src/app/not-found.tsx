0e1d0882e874188744f6d3c33f657944d7012200
shafay <muhammadshafay2014@gmail.com>
2025-05-29 20:28:47 +0500

    created the not found page recommended by the nextJs docs+ removed turbopack because sentry requires next version 15.3 in order to support turbopack

---
+++ src/app/not-found.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import { Space_Grotesk} from "next/font/google"
+import robot from "public/robot.jpg"
+
+export const metadata = {
+    title:"Page not found!"
+}
+
+const space = Space_Grotesk({
+  weight:"400",
+  subsets:["latin"]
+  
+})
+
+export default function NotFound() {
+  return (
+    <>
+        <div className="flex justify-center h-screen items-center mx-24 relative">
+      
+      
+              <h2 className={`${space.className} text-6xl `}>WE LOST THIS PAGE!</h2>
+    
+      <div className=" h-20 w-1 m-5 border-t-0 border-b-0 border-l-4 border-black " ></div>
+              <p className={`${space.className} text-5xl `}>Could not find requested resource</p>
+            <Image className="absolute -z-10 blur-xs" width="600" height="500" alt="404 background here" src={`/robot.jpg`}></Image>
+
+      </div>
+
+    </>
+  )
+}
