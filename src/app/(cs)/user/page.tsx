600cad4b4df7c51d959d9cc69b45e06f4ebb7e71
shafay <muhammadshafay2014@gmail.com>
2025-06-03 01:20:28 +0500

    changed metadata for the pages + updated to next 15.2.3

--- src/app/(cs)/user/page.tsx
+++ src/app/(cs)/user/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+
+export const metadata = {
+    title:"Customers"
+}
+
 export default function page() {
   return (
     <div className='home-fade text-2xl'>USER PAGE</div>
