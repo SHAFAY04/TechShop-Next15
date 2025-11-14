916b3fde4be7d0534a8732d426e801e72268565a
MUHAMMAD SHAFAY <117296248+SHAFAY04@users.noreply.github.com>
2025-11-14 20:44:54 +0500

    inventory-microservice: added entities to typeOrm.forFeature

--- backend/inventory-microservice/src/inventory/inventory.module.ts
+++ backend/inventory-microservice/src/inventory/inventory.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { productsEntity } from './entities/products.entity';
+import { products} from './entities/products.entity';
 import { inventoryController } from './inventory.controller';
 import { inventoryService } from './inventory.service';
+import { MonitorarmsDescription } from './entities/armsDescription.entity';
+import { category } from './entities/category.entity';
+import { GamingchairDescription } from './entities/chairsDescription.entity';
+import { channelProducts } from './entities/channelProducts.entity';
+import { CPUDescription } from './entities/cpuDescription.entity';
+import { GPUDescription } from './entities/gpuDescription.entity';
+import { KeyboardDescription } from './entities/keyboardDescription.entity';
+import { MouseDescription } from './entities/mouseDescription.entity';
+import { productImage } from './entities/productImages.entity';
+import { PowersupplyDescription } from './entities/psuDescription.entity';
+import { salesChannel } from './entities/salesChannel.entity';
+import { ScreenDescription } from './entities/screensDescription.entity';
 
 @Module({
-    imports:[TypeOrmModule.forFeature([productsEntity])],
+    imports:[TypeOrmModule.forFeature([products,MonitorarmsDescription,category,GamingchairDescription,channelProducts,CPUDescription,GPUDescription,KeyboardDescription,MouseDescription,productImage,PowersupplyDescription,salesChannel,ScreenDescription])],
     controllers:[inventoryController],
     providers:[inventoryService],
     exports:[]
